98eef4c5705e03f0b98d92ff37c6854e48c8166b
orekihoutaro <orekihoutaro1218@gmail.com>
2023-05-23 15:28:21 +0800

    Upload Component

---
+++ client/src/components/UploadComponent.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import axios from 'axios';
+import { useForm, useFieldArray } from 'react-hook-form';
+
+const UploadComponent = ({ userId }) => {
+  const { register, handleSubmit, control } = useForm({
+    defaultValues: {
+      images: [{ imageTitle: '', imageDescription: '', imageFile: null }],
+    },
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'images',
+  });
+
+  const onSubmit = async (data) => {
+    const formData = new FormData();
+
+    data.images.forEach((img, index) => {
+      formData.append(`images[${index}][title]`, img.imageTitle);
+      formData.append(`images[${index}][description]`, img.imageDescription);
+      formData.append(`images[${index}][file]`, img.imageFile[0]);
+    });
+
+    formData.append('uid', userId);
+    formData.append('title', data.title);
+    formData.append('description', data.description);
+
+    const response = await axios.post('http://localhost:3002/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    console.log(response.data);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen text-white bg-gray-900">
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md">
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-bold text-gray-300">
+            Project Title:
+          </label>
+          <input 
+            type="text" 
+            {...register("title")} 
+            required 
+            className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-bold text-gray-300">
+            Project Description:
+          </label>
+          <input 
+            type="text" 
+            {...register("description")} 
+            required 
+            className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        {fields.map((item, index) => (
+          <fieldset key={item.id} className="mb-4">
+            <div className="mb-2">
+              <label className="block mb-2 text-sm font-bold text-gray-300">
+                Image Title:
+              </label>
+              <input 
+                type="text" 
+                {...register(`images.${index}.imageTitle`)} 
+                required 
+                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            
+            <div className="mb-2">
+              <label className="block mb-2 text-sm font-bold text-gray-300">
+                Image Description:
+              </label>
+              <input 
+                type="text" 
+                {...register(`images.${index}.imageDescription`)} 
+                required 
+                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="block mb-2 text-sm font-bold text-gray-300">
+                Image:
+              </label>
+              <input 
+                type="file" 
+                {...register(`images.${index}.imageFile`)} 
+                required 
+                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              />
+            </div>
+
+            <button type="button" onClick={() => remove(index)} className="text-red-500">
+              Remove Image
+            </button>
+          </fieldset>
+        ))}
+
+        <button 
+          type="button" 
+          onClick={() => append({ imageTitle: '', imageDescription: '', imageFile: null })} 
+          className="text-blue-500"
+        >
+          Add Image
+        </button>
+
+        <div className="flex items-center justify-between mt-4">
+          <button 
+            type="submit"
+            className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+          >
+            Upload
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UploadComponent;
