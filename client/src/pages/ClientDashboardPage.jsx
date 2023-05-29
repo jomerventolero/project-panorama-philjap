9c594be55a9d4280aa3581313eefcb9a3bd465ac
orekihoutaro <orekihoutaro1218@gmail.com>
2023-05-29 09:33:11 +0800

    Client/User Side

---
+++ client/src/pages/ClientDashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ClientNavbar from '../components/ClientNavbar'
+
+const ClientDashboardPage = () => {
+  return (
+    <div>
+        <ClientNavbar />
+    </div>
+  )
+}
+
+export default ClientDashboardPage
