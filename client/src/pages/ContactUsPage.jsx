7f114c9fae2b5add0c1ad0ce14fd4fd8ea4c020d
orekihoutaro <orekihoutaro1218@gmail.com>
2023-05-30 09:16:45 +0800

    Initialize

--- client/src/pages/ContactUsPage.jsx
+++ client/src/pages/ContactUsPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import ClientNavbar from '../components/ClientNavbar'
+import EmailForm from '../components/EmailForm'
 
 const ContactUsPage = () => {
   return (
-    <div>
-      
+    <div className="flex items-center p-8">
+      <ClientNavbar />
+      <div className="flex flex-col items-center justify-center max-h-screen pt-20">
+        <EmailForm />
+      </div>
     </div>
   )
 }
