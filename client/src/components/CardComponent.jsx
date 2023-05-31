646e59f85a037b0d021492e047de45e4ea91e24a
orekihoutaro <orekihoutaro1218@gmail.com>
2023-05-31 22:28:51 +0800

    hotfix

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -20,16 +20,17 @@ const CardComponent = ({ project, onClick }) => {
 
   return (
     <div 
-      className="z-40 mx-auto overflow-hidden font-sans shadow-md cursor-pointer w-72 h-[500px] card-component bg-glass rounded-xl" 
+      className="z-40 mx-auto overflow-hidden font-sans shadow-md cursor-pointer w-72 h-[530px] card-component bg-glass rounded-xl" 
       onClick={onClick}
     >
       <div className="flex-col pt-8 md:flex">
         <div className="md:flex-shrink-0">
           {isLoading ? <Skeleton height={200} /> : <img className="object-cover w-full h-64 m-auto rounded-xl md:w-48" src={imageURL} alt={project.title} />}
         </div>
-        <div className="p-8">
+        <div className="p-8 flex flex-col justify-center items-center">
           <div className="font-semibold tracking-wide text-indigo-500 uppercase text-md">{project.title}</div>
           <p className="mt-2 text-sm text-left text-white">{project.description}</p>
+          <button className="p-2 m-2 bg-glass w-1/2 rounded-full text-white" onClick={onClick}>Open</button>
         </div>
       </div>
     </div>
