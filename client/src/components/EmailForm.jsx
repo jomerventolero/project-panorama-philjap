929cc90c4ad2b329db288707c28acaff650347a3
orekihoutaro <orekihoutaro1218@gmail.com>
2023-05-31 15:26:15 +0800

    Updates mga lodi

--- client/src/components/EmailForm.jsx
+++ client/src/components/EmailForm.jsx
@@ -94,6 +94,7 @@ const EmailForm = ({ userId }) => {
             name="to"
             value={to}
             onChange={handleInputChange}
+            disabled
             required
             className="w-full px-3 py-2 text-gray-200 bg-gray-800 border border-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
